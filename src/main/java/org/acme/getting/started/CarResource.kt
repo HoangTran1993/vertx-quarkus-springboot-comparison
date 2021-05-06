991bd35577005d4fbf7e84628efd09afd11c1c6d
hoangtn <hoangtn@vng.com.vn>
2021-05-06 18:07:31 +0700

    update

--- src/main/java/org/acme/getting/started/CarResource.kt
+++ src/main/java/org/acme/getting/started/CarResource.kt
@@ -1,6 +1,7 @@
 package org.acme.getting.started
 
 import com.google.gson.Gson
+import org.acme.getting.started.model.Car
 import org.acme.getting.started.service.DataService
 import javax.inject.Inject
 import javax.ws.rs.core.MediaType
@@ -14,23 +15,21 @@ class CarResource {
     lateinit var responseStream: CarStreamResponseOutput
 
     @GET
-    @Produces("application/json")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     fun getCarsAsList() : Response {
         val blockingGet = DataService.getDataStream(0).toList().blockingGet()
         println(blockingGet)
-        val gson = Gson()
-        return Response.ok(gson.toJson(blockingGet)).build()
+        return Response.ok(blockingGet).build()
     }
 
     @GET
-    @Path("/async1")
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
     fun loadCarsAsOctetStream(): Response {
         return Response.ok().entity(responseStream).build()
     }
 
     @GET
-    @Path("/async2")
     @Produces("application/stream+json")
     fun loadCarsAsJsonStream(): Response {
         return Response.ok().entity(responseStream).build()
